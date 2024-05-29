819251b521fd9ac486654ac60aa8224f131952d2
janavalsan55 <janavalsan@gmail.com>
2024-05-29 12:55:02 +0530

    responsive

--- assets/header.js
+++ assets/header.js
@@ -1,11 +1,11 @@
-
-    $(document).ready(function() {
-        $('.mega-menu-content').hide();
-        $('.mega-menu').hover( function() {
-            $(this).find('.mega-menu-content').stop(true, true).slideDown();
-          },
-          function() {
-            $(this).find('.mega-menu-content').stop(true, true).slideUp();
-          }
-        );
-      });
+$(document).ready(function() {
+  $('.mega-menu-content').hide();
+  $('.mega-menu').hover(
+      function() {
+          $(this).find('.mega-menu-content').stop(true, true).fadeIn();
+      },
+      function() {
+          $(this).find('.mega-menu-content').stop(true, true).fadeOut();
+      }
+  );
+});
