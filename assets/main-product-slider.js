cab0b438935a83a18a5fff581c8e214df48db2e5
janavalsan55 <janavalsan@gmail.com>
2024-06-10 16:25:18 +0530

    slider editing

--- assets/main-product-slider.js
+++ assets/main-product-slider.js
@@ -1,26 +1,49 @@
 $(document).ready(function(){
-  var sliderFor = $('.slider-for').slick({
-    verticalSwiping: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
+    var sliderFor = $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        asNavFor: '.slider-nav',
+        infinite: false,
+        centerMode: true, 
+        centerPadding: 0, 
+        vertical: true,
+        verticalSwiping: true,
+        swipeToSlide: true,
+        focusOnSelect: true
+    });
+    
+    var sliderNav = $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        focusOnSelect: true,
+        arrows:true,
+        prevArrow: '.slick-prev', 
+        nextArrow: '.slick-next',
+        infinite: false 
+    });
+  
+    function handleScroll(slider, e) {
+        e.preventDefault();
+        var currentSlide = slider.slick('slickCurrentSlide');
+        var slideCount = slider.slick('getSlick').slideCount;
 
-  var sliderNav = $('.slider-nav').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    focusOnSelect: true
-  });
-
-  sliderFor.on('wheel', function(e) {
-    e.preventDefault();
-    if (e.originalEvent.deltaY < 0) {
-      $(this).slick('slickNext');
-    } else {
-      $(this).slick('slickPrev');
+        if (e.originalEvent.deltaY < 0) {
+            // Scroll up
+            if (currentSlide > 0) {
+                slider.slick('slickPrev');
+            }
+        } else {
+            // Scroll down
+            if (currentSlide < slideCount - 1) {
+                slider.slick('slickNext');
+            }
+        }
     }
-  });
+
+    sliderFor.on('wheel', function(e) {
+        handleScroll(sliderFor, e);
+    });
 });
